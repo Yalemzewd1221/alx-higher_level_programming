2788fd3928bd05997b7223dfd16df62a01fa74e2
Meseret <meseretmaru.wassie@gmail.com>
2023-06-13 17:19:19 -0700

    tewelve

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Student class"""
+"""Defines a function that returns a list of lists containing
+N sized pascal's triangle"""
 
 
-class Student:
-    """Student class"""
-
-    def __init__(self, first, last, age):
-        self.first_name = first
-        self.last_name = last
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Returns student's serializable dict elements as a dict"""
-        filterattr = 0
-        if type(attrs) == list:
-            filterattr = 1
-            for x in attrs:
-                if type(x) is not str:
-                    filterattr = 0
-                    break
-        retdict = {}
-        objdict = self.__dict__
-        for ele in objdict:
-            if type(objdict[ele]) in [list, dict, str, int, bool]:
-                if filterattr == 0 or ele in attrs:
-                    retdict[ele] = objdict[ele]
-        return retdict
+def pascal_triangle(n):
+    """Returns a pascal's triangle of size n"""
+    triangle = []
+    if n <= 0:
+        return []
+    n -= 1
+    for row in range(n + 1):
+        triangle.append([])
+        triangle[row].append(1)
+        for col in range(int(row / 2)):
+            triangle[row].append(triangle[row - 1][col] +
+                                 triangle[row - 1][col + 1])
+        if row % 2 == 1:
+            triangle[row].extend(triangle[row][::-1])
+        else:
+            triangle[row].extend(triangle[row][-2::-1])
+    return triangle
