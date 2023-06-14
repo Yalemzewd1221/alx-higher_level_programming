575364adca0aae24bf9604361430461d7afa5b81
Meseret <meseretmaru.wassie@gmail.com>
2023-06-13 17:13:55 -0700

    d

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that loads an object from a json string"""
+"""Defines a function that loads an object from a file's json string"""
 import json
 
 
-def from_json_string(my_str):
-    """Converts json string to an object"""
-    return json.loads(my_str)
+def load_from_json_file(filename=""):
+    with open(filename, "r") as f:
+        return json.load(f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Defines a function that saves an object to a file as json string"""
+"""Makes a list from args and saves them to a file in json format"""
+import sys
 import json
+savejson = __import__("7-save_to_json_file").save_to_json_file
+loadjson = __import__("8-load_from_json_file").load_from_json_file
 
-
-def save_to_json_file(my_obj, filename):
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+try:
+    listy = loadjson("add_item.json")
+except FileNotFoundError:
+    listy = []
+for arg in sys.argv[1:]:
+    listy.append(arg)
+savejson(listy, "add_item.json")
