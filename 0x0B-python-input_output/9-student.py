1e9250d91fc0a3a0734344cce88c6dfe05453286
Meseret <meseretmaru.wassie@gmail.com>
2023-06-13 17:29:12 -0700

    askorotu

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Makes a list from args and saves them to a file in json format"""
-import sys
-import json
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
+"""Student class"""
 
-try:
-    listy = loadjson("add_item.json")
-except FileNotFoundError:
-    listy = []
-for arg in sys.argv[1:]:
-    listy.append(arg)
-savejson(listy, "add_item.json")
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first, last, age):
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+
+    def to_json(self):
+        """Returns student's serializable dict elements as a dict"""
+        retdict = {}
+        objdict = self.__dict__
+        for ele in objdict:
+            if type(objdict[ele]) in [list, dict, str, int, bool]:
+                retdict[ele] = objdict[ele]
+        return retdict
